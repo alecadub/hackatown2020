cf5cbab9fdc37bc8023b39cabb248dce45411863
Alec Adub <adub.alec@gmail.com>
2020-01-18 13:11:57 -0500

    add agm-directions

--- hackatown2020/src/app/tab2/components/google-map/google-map.component.ts
+++ hackatown2020/src/app/tab2/components/google-map/google-map.component.ts
@@ -9,13 +9,17 @@ import { Platform } from '@ionic/angular';
 export class GoogleMapComponent implements OnInit {
   lat: number;
   lng: number;
+  origin: any;
+  destination: any;
 
   height: number;
 
   constructor(public platform: Platform) {
-    this.lat = 51.678418;
-    this.lng = 7.809007;
+    this.lat = 24.799448;
+    this.lng = 120.979021;
     this.height = platform.height();
+    this.origin = { lat: 24.799448, lng: 120.979021 };
+    this.destination = { lat: 24.799524, lng: 120.975017 };
   }
 
   ngOnInit() {}
