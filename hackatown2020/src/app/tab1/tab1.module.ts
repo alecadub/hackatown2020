008ae9e3049806e6fdd07a4e21b12352ba36440b
Philippe Merariu <lmerariu@gmail.com>
2020-01-18 16:00:58 -0500

    Tab 1 - basic UI

--- hackatown2020/src/app/tab1/tab1.module.ts
+++ hackatown2020/src/app/tab1/tab1.module.ts
@@ -4,12 +4,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { AgmCoreModule } from '@agm/core';
+import { APIkey } from 'src/environments/key';
+import { AgmDirectionModule } from 'agm-direction';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: APIkey
+    }),
+    AgmDirectionModule,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
   declarations: [Tab1Page]
